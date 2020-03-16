4aa85daec3ea47650328b0202792abb532603341
Sebastien Lemieux <s.lemieux@umontreal.ca>
2020-03-16 19:18:55 -0400

    Expanded the definition of MerTable. Experimentation with parametric types (now working!). Primitive hash table (with collision). 6.8 s/M (without checks).

--- prepare_table.jl
+++ prepare_table.jl
@@ -1,8 +1,12 @@
+
+## Aim for 28.8 sec for a 1M reads. This will give 15m for 250M reads on 8 threads.
+
 include("src/MerTable.jl")
 
 cd("C:\\Users\\slemi\\prog\\Load")
 
 fn = "../10H005/10H005_ACTTGA_L001_R1_001.fastq.gz"
 
-@time d = build_ktable(fn)
+d = MerTable{UInt64}(Int(1e8))
+@time build_mertable!(fn, d)
 print(d)
--- src/MerTable.jl
+++ src/MerTable.jl
@@ -3,25 +3,45 @@ using CodecZlib
 using Printf
 using BioSequences
 
-# import Base.hash
-# function hash(x::DNAMer{31})::UInt64
-#     return convert(UInt64,x)
-# end
+mutable struct MerTable{E}
+    data::Array{E} # The k-mer stored
+    count::Array{Int}   # The number observed
+    collision::Int      # Nb. of collision
+    unique::Int         # Total number of unique k-mer observed
+    capacity::Int       # Size of the arrays
+end
 
-function build_ktable(fn)
-    d = Dict{DNAMer{31},Int}()
+function MerTable{E}(capacity) where {E}
+    return MerTable(zeros(E, capacity), zeros(Int, capacity),
+                    0, 0, capacity)
+end
+
+function record!(t::MerTable{E}, m::UInt64) where {E}
+    i = m % t.capacity + 1
+    t.data[i] = m
+    if t.count[i] == 0
+        t.unique += 1
+    elseif m != t.data[i]
+        t.collision += 1
+    end
+    t.count[i] += 1
+end
+
+function Base.show(io::IO, t::MerTable{E}) where {E}
+    println(io, "Capacity: ", t.capacity)
+    println(io, "Collision: ", t.collision, " (", t.collision / t.capacity * 100, "%)")
+    println(io, "Unique (M): ", t.unique / 1e6)
+    println(io, "Occupancy: ", t.unique / t.capacity * 100, "%")
+end
+
+function build_mertable!(fn, d::MerTable)
     count_seq = 0
     count_k = 0
     for record in open(fn) |> GzipDecompressorStream |> FASTQ.Reader
         s = FASTQ.sequence(record)
         for m in each(DNAMer{31}, s)
-            c = fwmer(m)
-            count_k += haskey(d, c)
-            # if haskey(d, c)
-            #     d[c] += 1
-            # else
-            #     d[c] = 1
-            # end
+            c = convert(UInt64, fwmer(m))
+            record!(d, c)
         end
         count_seq += 1
         if count_seq > 1e6
